57a9bed406df9cbd3b3c9f4484b9fbb995612d6c
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-09-08 21:14:03 +0530

    Update SumOfNumbers2.java

--- java/SumOfNumbers2.java
+++ java/SumOfNumbers2.java
@@ -7,7 +7,7 @@ public static void main(String args[])
 {  
 int x, y, sum;  
 Scanner sc = new Scanner(System.in);  
-System.out.print("Enter the first number: ");  
+System.out.print("Enter the first number:  ");  
 x = sc.nextInt();  
 System.out.print("Enter the second number: ");  
 y = sc.nextInt();  
