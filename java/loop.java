668fac1e7f513076cdad3b6250b376662472ca1c
Manas-tech <manastechnonjr@gmail.com>
2023-09-23 21:22:19 +0530

    Add files via upload

---
+++ java/loop.java
@@ -0,0 +1,11 @@
+// Program to print a text 5 times
+
+class Main {
+  public static void main(String[] args) {
+
+    int n = 69;
+    for (int i = 1; i <= n; ++i) {
+      System.out.println("THIS IS IN LOOP");
+    }
+  }
+}
